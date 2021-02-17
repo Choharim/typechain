fc3935d09c534f57d3e438b73f656949ec7a2694
Choharim <jhl8586@naver.com>
2021-02-17 15:34:59 +0900

    Add tsc-watch

---
+++ dist/index.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var name = "mori";
+var gender = "female";
+var age = 8;
+var sayHi = function (name, gender, age) {
+    console.log("hello, my name is " + name + ", i am a " + gender + " and i am " + age + "!!!!.");
+};
+sayHi(name, gender, age);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+const name:string = "mori";
+const gender:string = "female";
+const age:number = 8;
+
+const sayHi = (name:string,gender:string,age:number) => {
+  console.log(`hello, my name is ${name}, i am a ${gender} and i am ${age}!!!!.`);
+};
+sayHi(name,gender,age);
+export {};
